6af80270773c1629b0efba96b64d2a901eb460c2
nov05 <you@example.com>
2025-05-07 22:53:14 -0500

    following youtube tutorials

---
+++ misc/01_math.jl
@@ -0,0 +1,22 @@
+## created by nov05 on 2025-05-07
+#=
+    This file includes example code from the following video.
+    https://www.youtube.com/watch?v=KlorfxsdWDw
+=#
+## Alt+J+O to open the Julia REPL terminal
+## Ctrl+Enter to evalute a line
+
+1 + 1
+5 / 2
+2^3
+## 5 modulo 2
+5 % 2
+1 + (2-3)^4 * 5
+
+## Boolean
+3 > 2
+3 < 2
+4 >= 3
+4 == 3
+(1==1) && (1>1)
+(1==1) || (1>1)
